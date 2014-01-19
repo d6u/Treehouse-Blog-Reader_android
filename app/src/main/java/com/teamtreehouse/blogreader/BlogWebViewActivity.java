7208de750948c4af18891a790fd1cb81f92ac3d8
Daiwei Lu <daiweilu123@gmail.com>
2014-01-19 11:43:06 -0500

    Add share intent

--- app/src/main/java/com/teamtreehouse/blogreader/BlogWebViewActivity.java
+++ app/src/main/java/com/teamtreehouse/blogreader/BlogWebViewActivity.java
@@ -4,9 +4,14 @@ import android.app.Activity;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.webkit.WebView;
 
 public class BlogWebViewActivity extends Activity {
+
+    protected String mUrl;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,14 +20,35 @@ public class BlogWebViewActivity extends Activity {
 
         Intent intent = getIntent();
         Uri blogUri = intent.getData();
+        mUrl = blogUri.toString();
 
         WebView webView = (WebView) findViewById(R.id.webView1);
-        webView.loadUrl(blogUri.toString());
+        webView.loadUrl(mUrl);
     }
 
-//    public boolean onCreateOptionsMenu(Menu menu) {
-//        getMenuInflater().inflate(R.menu.activity_blog_web_view, menu);
-//        return true;
-//    }
 
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.activity_blog_web_view, menu);
+        return true;
+    }
+
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int itemId = item.getItemId();
+
+        if (itemId == R.id.action_share) {
+            sharePosts();
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+
+    private void sharePosts() {
+        Intent shareIntent = new Intent(Intent.ACTION_SEND);
+        shareIntent.setType("text/plain");
+        shareIntent.putExtra(Intent.EXTRA_TEXT, mUrl);
+        startActivity(Intent.createChooser(shareIntent, "How do you want to share?"));
+    }
 }
